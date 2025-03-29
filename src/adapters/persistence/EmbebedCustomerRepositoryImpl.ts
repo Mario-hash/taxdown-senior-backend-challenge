542c8a4b9845cd919a1ff6c70f74d49d20800d5e
Odd1ty <mariofernandezsantos@gmail.com>
2025-03-29 18:51:46 +0100

    feat(repository): Implemented all methods to embebed customer repo to pass test

--- src/adapters/persistence/EmbebedCustomerRepositoryImpl.ts
+++ src/adapters/persistence/EmbebedCustomerRepositoryImpl.ts
@@ -12,4 +12,17 @@ export class EmbebedCustomerRepository implements ICustomerRepository {
   async findById(customerId: string): Promise<Customer | null> {
     return this.customers.get(customerId) || null;
   }
+
+  async update(customer: Customer): Promise<Customer> {
+    this.customers.set(customer.id, customer);
+    return customer;
+  }
+
+  async delete(customerId: string): Promise<void> {
+    this.customers.delete(customerId);
+  }
+
+  async findAll(): Promise<Customer[]> {
+    return Array.from(this.customers.values());
+  }
 }
