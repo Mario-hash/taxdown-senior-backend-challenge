7d413ce2cb0f5da9e89b4bd075d4f4b5f0189cce
Odd1ty <mariofernandezsantos@gmail.com>
2025-04-03 13:55:10 +0200

    feat(pipeline): Added test environment option to to mongo instance

--- src/adapters/persistence/MongoClientFactory.ts
+++ src/adapters/persistence/MongoClientFactory.ts
@@ -1,15 +1,30 @@
 import { MongoClient, Db, Collection } from 'mongodb';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 import { CustomerPersistence } from './dto/CustomerPersistence';
 
-const uri = 'mongodb://localhost:27017'; // Cambia si lo necesitas
-const dbName = 'taxdown';
 let client: MongoClient;
+let mongod: MongoMemoryServer | null = null;
 
 export const getMongoCollection = async (): Promise<Collection<CustomerPersistence>> => {
-  if (!client) {
+  if (process.env.NODE_ENV === 'test') {
+    if (!mongod) {
+      mongod = await MongoMemoryServer.create();
+    }
+    const uri = mongod.getUri();
     client = new MongoClient(uri);
     await client.connect();
+  } else {
+    if (!client) {
+      const uri = 'mongodb://localhost:27017';
+      client = new MongoClient(uri);
+      await client.connect();
+    }
   }
-  const db: Db = client.db(dbName);
+  const db: Db = client.db(process.env.MONGO_DB_NAME || 'taxdown');
   return db.collection<CustomerPersistence>('customers');
+};
+
+export const closeMongoConnection = async (): Promise<void> => {
+  if (client) await client.close();
+  if (mongod) await mongod.stop();
 };
